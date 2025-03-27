c3c7372c3a9337739e74a355f682b3d9f54df5a8
stoyan-dev <xuij777@gmail.com>
2025-03-27 12:27:15 +0100

    Adding interface and fetching functionality from the api

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+}
 
 export const UserList: React.FC = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+
   return (
     <div>
       <h2>Users</h2>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
